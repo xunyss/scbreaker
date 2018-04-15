d19b879d6004119bb46d7d62455452c089210135
xunyss@gmail.com <xunyss@gmail.com>
2018-04-15 18:23:23 +0900

    init

---
+++ src/main/java/io/xunyss/scbreaker/ScBreaker.java
@@ -0,0 +1,10 @@
+package io.xunyss.scbreaker;
+
+import io.xunyss.commons.lang.StringUtils;
+
+public class ScBreaker {
+	
+	public static void main(String[] args) {
+		System.out.println("hello world" + StringUtils.EMPTY);
+	}
+}
